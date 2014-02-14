bb93e5db81d3e46cfe282b1388c5a19f21938a21
Mark McEahern <mark@mceahern.com>
2014-02-14 16:49:07 -0600

    First attempt at Control middleware

--- lib/floodgate.rb
+++ lib/floodgate.rb
@@ -1,4 +1,20 @@
 require 'floodgate/version'
 
 module Floodgate
+  class Control
+    def initialize(app, filter_traffic=false)
+      @filter_traffic = filter_traffic
+      @app = app
+    end
+
+    def call(env)
+      return @app.call(env) unless @filter_traffic
+
+      if maintenance_url = env['MAINTENANCE_PAGE_URL']
+        [307, { 'Location' => maintenance_url }, []]
+      else
+        [503, {}, ['Application Unavailable']]
+      end
+    end
+  end
 end
--- spec/floodgate_spec.rb
+++ spec/floodgate_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper'
 
-describe Floodgate do
+module Floodgate
+  describe Control do
+    let(:app) { double().as_null_object }
+    let(:control) { described_class.new(app) }
+    let(:env) { Hash.new }
+
+    describe '#call' do
+      it 'sends :call to the app with the specified environment' do
+        expect(app).to receive(:call).with(env)
+        control.call(env)
+      end
+
+      context 'when the floodgate is closed' do
+        let(:filter_traffic) { true }
+        let(:control) { described_class.new(app, filter_traffic) }
+
+        it 'does not send :call to the app' do
+          expect(app).not_to receive(:call).with(env)
+          control.call(env)
+        end
+
+        context 'when a maintenance page is specified in the environment' do
+          before { env['MAINTENANCE_PAGE_URL'] = 'someurl' }
+
+          it 'redirects with a temporary redirect status' do
+            expect(control.call(env)[0]).to eq(307)
+          end
+
+          it 'has a location set to the maintenance page' do
+            expect(control.call(env)[1]['Location']).to eq('someurl')
+          end
+        end
+
+        context 'when no maintenance page is specified in the environment' do
+          it 'responds with a status of service unavailable' do
+            expect(control.call(env)[0]).to eq(503)
+          end
+
+          it 'has an appropriate message in the response' do
+            expect(control.call(env)[2]).to eq(['Application Unavailable'])
+          end
+        end
+      end
+    end
+  end
 end
