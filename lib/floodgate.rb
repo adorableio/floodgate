f4a246b5aa2c7146f74b1db3afdaf85762d40fc3
Mark McEahern <mark@mceahern.com>
2014-02-25 11:33:10 -0600

    Organization of files

--- lib/floodgate.rb
+++ lib/floodgate.rb
@@ -1,36 +1,4 @@
-require 'floodgate/version'
 require 'floodgate/config'
-
-module Floodgate
-  class Control
-    attr_accessor :app, :config
-
-    def initialize(app, config)
-      @app = app
-      @config = config
-    end
-
-    def call(env)
-      return app.call(env) unless filter_traffic?(env)
-
-      if redirect?
-        [307, { 'Location' => redirect_url }, []]
-      else
-        [503, {}, ['Application Unavailable']]
-      end
-    end
-
-    def filter_traffic?(env)
-      config.filter_traffic?(env)
-    end
-
-    def redirect?
-      config.redirect?
-    end
-
-    def redirect_url
-      config.redirect_url
-    end
-  end
-end
+require 'floodgate/control'
+require 'floodgate/version'
 
---
+++ lib/floodgate/control.rb
@@ -0,0 +1,33 @@
+module Floodgate
+  class Control
+    attr_accessor :app, :config
+
+    def initialize(app, config)
+      @app = app
+      @config = config
+    end
+
+    def call(env)
+      return app.call(env) unless filter_traffic?(env)
+
+      if redirect?
+        [307, { 'Location' => redirect_url }, []]
+      else
+        [503, {}, ['Application Unavailable']]
+      end
+    end
+
+    def filter_traffic?(env)
+      config.filter_traffic?(env)
+    end
+
+    def redirect?
+      config.redirect?
+    end
+
+    def redirect_url
+      config.redirect_url
+    end
+  end
+end
+
