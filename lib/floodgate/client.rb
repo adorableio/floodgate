661ee50253ea211498d291dcbc3c64be816172af
Mark McEahern <mark@mceahern.com>
2014-02-26 11:15:38 -0600

    Update to latest api: ssl, api_token

--- lib/floodgate/client.rb
+++ lib/floodgate/client.rb
@@ -5,7 +5,7 @@ module Floodgate
   class Client
     def self.status(app_id, api_token)
       # TODO: Handle errors, timeouts, proxies, environments, etc.
-      JSON.parse(open("http://floodgate-api-staging.herokuapp.com/api/v1/apps/#{app_id}/status?token=#{api_token}").read)
+      JSON.parse(open("https://floodgate-api-staging.herokuapp.com/api/v1/apps/#{app_id}/status?api_token=#{api_token}").read)
     end
   end
 end
